8b78d1202b20f3d74bcc299985733bf8ca70cade
Chirag Patel <chiragp2303@gmail.com>
2026-08-05 22:59:13 +0530

    asddffzxccqwertty

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-import express from 'express'
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+
 import cors from 'cors'
-import 'dotenv/config'
+
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
